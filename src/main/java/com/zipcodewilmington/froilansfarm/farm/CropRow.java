dbd496d0a7fbb07f835af250d2be351def1cfab4
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-27 18:04:50 -0500

    CropRow methods, tests set up

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/CropRow.java
@@ -1,4 +1,38 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import com.zipcodewilmington.froilansfarm.crop.Crop;
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CropRow extends Field {
+    private List<Crop> cropRow;
+
+    public CropRow() {
+        this.cropRow =  new ArrayList<>();
+    }
+
+    public void addCrop(Crop crop) {
+        cropRow.add(crop);
+    }
+
+    public void removeCrop(Crop crop) {
+        cropRow.remove(crop);
+    }
+
+    public void fertilizeCropRow() {
+        for(Crop crop : cropRow) {
+            crop.setHasBeenFertilized(true);
+        }
+    }
+
+    public List<Edible> harvestCropRow() {
+        List<Edible> edibleList = new ArrayList<>();
+
+        for(Crop crop : cropRow) {
+            edibleList.add(crop.yield());
+        }
+        return edibleList;
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/farm/CropRowTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/farm/CropRowTest.java
@@ -1,4 +1,30 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import org.junit.Test;
+
 public class CropRowTest {
+    @Test
+    public void testConstructor() {
+
+    }
+
+    @Test
+    public void testAddCrop() {
+
+    }
+
+    @Test
+    public void testRemoveCrop() {
+
+    }
+
+    @Test
+    public void testFertilizeCropRow() {
+
+    }
+
+    @Test
+    public void testHarvestCropRow() {
+
+    }
 }
