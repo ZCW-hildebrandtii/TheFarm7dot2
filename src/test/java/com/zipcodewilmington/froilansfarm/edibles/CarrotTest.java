fd5f140bc564c4ade95aa8d2c2436ed22a2db04d
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-28 12:11:19 -0500

    Edibles Tests

--- src/test/java/com/zipcodewilmington/froilansfarm/edibles/CarrotTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/edibles/CarrotTest.java
@@ -1,4 +1,38 @@
 package com.zipcodewilmington.froilansfarm.edibles;
 
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CarrotTest {
+
+    @Test
+    public void testConstructor() {
+        // Given
+        Carrot carrot = new Carrot();
+
+        // Then
+        Assert.assertTrue(carrot.isEdible());
+    }
+
+    @Test
+    public void testInstanceOfEdible() {
+        // Given
+        Carrot carrot = new Carrot();
+
+        // Then
+        Assert.assertTrue(carrot instanceof Edible);
+    }
+
+    @Test
+    public void testSetEdible() {
+        // Given
+        Carrot carrot = new Carrot();
+
+        // When
+        carrot.setEdible(false);
+
+        // Then
+        Assert.assertFalse(carrot.isEdible());
+    }
 }
